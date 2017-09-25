17509b6ec95304974ff89d3b2745e7281bc0cc3e
FerDoranNie <fertimjim@gmail.com>
2017-09-25 18:07:47 -0500

    Iniciando el repositorio :ant:

---
+++ SeatPronosticos2018.R
@@ -0,0 +1,233 @@
+####################################
+#Creado por Fernando Dorantes Nieto
+#                                   <(°) 
+#                                     ( >)"
+#                                      /|
+####################################
+
+
+library(magrittr)
+c("dplyr","tidyr","lubridate","tseries", "astsa","forecast","ggplot2", 
+  "lattice", "dygraphs","XLConnect", "palettetown", "xts", "Rfacebook",
+  "twitteR") %>%  
+  sapply(require, character.only=T)
+
+
+
+
+setwd("~/local/TimeSeries18/")
+
+
+# Funciones ---------------------------------------------------------------
+
+"%!in%" <- function(x,y)!("%in%"(x,y))
+
+# Datos -------------------------------------------------------------------
+fb <- read.csv("datos/facebook/datosHomologadosFB.csv",
+               header = T)
+
+tw <- read.csv("datos/twitter/datosHomologadosTW.csv")
+
+ins <- read.csv("datos/instagram/datosHomologadosIN.csv")
+
+yt <- read.csv("datos/youtube/datosHomologadosYT.csv")
+
+
+######## ENGANCHE
+interaccionesEnganche <- c(443306,410142, 364830,359057,491580,466709,453217,
+                           369618,308420, 263196,184779,94236,74403,74767,129481,
+                           144873,160474,177282,228588,176236,177472,255553,94847,
+                           77876,121251,162493,92327,81270,66835,
+                           75607,81879,77246,50976,67341,64934,63627,
+                           69444,70018, 63454,57732,
+                           57065, 64103,66721, 55491, 53966, 54594,52520,
+                           49986,49121,55990, 46112, 43016, 44710, 30034,
+                           32858, 43748)
+
+FansTotales <- c(356210, 444359, 524536, 614642,694754, 766517, 809811, 834032, 
+                878055, 960524, 993974,1067261, 1070950, 1124330, 1170256, 
+                1225499, 1266445,1303574,1362823, 1429278, 1477808,1559615, 
+                1572561,1576928, 1580582, 1590939, 1591753,1551294, 1556634,
+                1559181, 1558787,1560633,1562967,1564392,1566641,1572749,
+                1574437,1574426,1576277, 1551294, 1556634, 1559181,1558787,
+                1560633, 1562967,1599785,1604375,1611016,1615297,1624452,
+                1632920, 1651192, 1662813, 1672601, 1674674, 1681819)
+
+
+
+# Globales ----------------------------------------------------------------
+
+fechaFinal <- as.Date("2019-01-01")
+
+
+
+# Pronósticos facebook ----------------------------------------------------
+
+###FANS TOTALES
+totalesFansDiario <- xts(unique(fb[,2]), as.Date(unique(fb[,1]))) 
+
+secuenciaTotales <- seq_along(totalesFansDiario)
+
+modeloLineal <- lm(coredata(totalesFansDiario) ~ secuenciaTotales) 
+diferenciaLineal <- difftime(fechaFinal, time(last(totalesFansDiario)))
+diferenciaLineal <- ceiling(as.numeric(diferenciaLineal))
+
+Time_predLineal <- 1:(length(totalesFansDiario) + (diferenciaLineal))
+
+predLineal <- predict(modeloLineal, 
+                      newdata = list(secuenciaTotales = Time_predLineal))
+
+predLineal <- xts(predLineal, seq(ymd(20160101), by="day", 
+                                  length.out = length(predLineal)))
+
+cbind(totalesFansDiario, predLineal) %>%  
+  dygraph()
+
+
+############ ENGANCHE #####################
+fechas = seq(ymd('2013-01-01'),ymd('2017-08-01'), by = 'months')
+suma = ((interaccionesEnganche)/FansTotales)*100
+engage = xts(suma,fechas)
+Tiempo = seq_along(engage)
+
+expFct <- function(x, beta1, beta2, beta3){exp(-beta1 * x)/(beta2 + beta3 * x)}
+
+###### Modelo
+nolineal <- nls(coredata(engage)~expFct(x = Tiempo ,beta1, beta2, beta3),
+                start = list(beta1 = 0.1, beta2 = 0.02, beta3 = 0.03))
+
+
+diferenciaEngage <- difftime(fechaFinal, last(fechas))
+diferenciaEngage <- as.numeric(diferenciaEngage)/30
+diferenciaEngage <- floor(diferenciaEngage)-1
+
+TIME_PRED <- 1:(length(Tiempo) + diferenciaEngage)
+
+pred.en <- xts(predict(nolineal, newdata = list(Tiempo = TIME_PRED)),
+              seq(ymd(20130101), by = "month", length.out = length(engage) + 
+                    diferenciaEngage))
+
+engagement <- cbind(engage, pred.en)
+names(engagement) <- c("Observado","No lineal")
+tiempoe <- rep(time(engagement), 2)
+grafoEn <- cbind(engage %>%  na.omit(), pred.en )
+names(grafoEn) <- c("Engagement", "Estimación (No lineal)")
+
+ggGrafoEn <- data.frame(fecha = time(grafoEn), 
+                       data.frame(grafoEn))
+
+observadoAgos <- engage["2017-08-01"] %>% 
+  as.numeric %>% 
+  round(digits=2)
+
+enero <- pred.en["2018-01-01"]  %>% 
+  as.numeric %>% 
+  round(digits=2)
+
+junio <- pred.en["2018-06-01"] %>% 
+  as.numeric %>% 
+  round(digits=2)
+
+diciembre <- pred.en["2018-12-01"] %>% 
+  as.numeric %>% 
+  round(digits=2)
+
+correlacion <- cor(coredata(engage), predict(nolineal))
+
+ggGrafoEn %>% 
+  gather(tipo, valor, -fecha) %>% 
+  ggplot(aes(x = fecha, y=valor, color=tipo))+
+  geom_line()+
+  geom_point()+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle=45, hjust=1),
+        legend.position = "top")+
+  scale_color_manual(values = c("darkred", "steelblue"),
+                     name="", labels=c("Observado", "Estimación"))+
+  scale_x_date(date_breaks = "2 months")+
+  scale_y_continuous(limits = c(0,130), breaks=seq(0, 130, 10))+
+  xlab("")+
+  ylab("Enganche %")+
+  geom_vline(xintercept = as.numeric(as.Date("2017-08-01")),
+             linetype=4, color="darkred")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-01-01")),
+             linetype=4, color="steelblue")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-06-01")),
+             linetype=4, color="steelblue")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-12-01")),
+             linetype=4, color="steelblue")+
+  geom_text(aes(x = as.Date('2017-07-01'), y= 100, 
+                label= paste('Observado', "2017-08-01",  
+                             observadoAgos, "%",  sep="\t")), angle=90,
+            color="darkred")+ 
+  geom_text(aes(x = as.Date('2017-12-01'), y= 100, 
+                label= paste('Pronóstico', '2018-01-01',  
+                             enero, "%",  sep="\t")), angle=90) +
+  geom_text(aes(x = as.Date('2018-05-01'), y= 100, 
+                label= paste('Pronóstico', '2018-06-01',  
+                             junio, "%",  sep="\t")), angle=90) +
+  geom_text(aes(x = as.Date('2018-11-01'), y= 100, 
+                label= paste('Pronóstico', '2018-12-01',  
+                             diciembre, "%",  sep="\t")), angle=90) 
+  
+
+
+ggGrafoEn %>% 
+  filter(fecha>"2015-01-01") %>% 
+  gather(tipo, valor, -fecha) %>% 
+  ggplot(aes(x = fecha, y=valor, color=tipo))+
+  geom_line()+
+  geom_point()+
+  theme_classic()+
+  theme(axis.text.x = element_text(angle=45, hjust=1),
+        legend.position = "top") +
+  scale_color_manual(values = c("darkred", "steelblue"),
+                     name="", labels=c("Observado", "Estimación"))+
+  scale_x_date(date_breaks = "2 months")+
+  scale_y_continuous(limits = c(0,12), breaks=1:12)+
+  xlab("")+
+  ylab("Enganche %")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-01-01")) )+
+  geom_vline(xintercept = as.numeric(as.Date("2017-08-01")),
+             linetype=4, color="darkred")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-01-01")),
+             linetype=4, color="steelblue")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-06-01")),
+             linetype=4, color="steelblue")+
+  geom_vline(xintercept = as.numeric(as.Date("2018-12-01")),
+             linetype=4, color="steelblue")+
+  geom_text(aes(x = as.Date('2017-07-01'), y= 7, 
+                label= paste('Observado', "2017-08-01",  
+                             observadoAgos, "%",  sep="\t")), angle=90,
+            color="darkred")+ 
+  geom_text(aes(x = as.Date('2017-12-01'), y= 7, 
+                label= paste('Pronóstico', '2018-01-01',  
+                             enero, "%",  sep="\t")), angle=90) +
+  geom_text(aes(x = as.Date('2018-05-01'), y= 7, 
+                label= paste('Pronóstico', '2018-06-01',  
+                             junio, "%",  sep="\t")), angle=90) +
+  geom_text(aes(x = as.Date('2018-11-01'), y= 7, 
+                label= paste('Pronóstico', '2018-12-01',  
+                             diciembre, "%",  sep="\t")), angle=90) 
+
+
+
+
+
+
+
+# Pronósticos twitter -----------------------------------------------------
+
+
+
+
+# Pronósticos YouTube -----------------------------------------------------
+
+
+
+
+# Pronósticos sitio web ---------------------------------------------------
+
+
+
+
---
+++ datosHomologa.R
@@ -0,0 +1,147 @@
+####################################
+#Creado por Fernando Dorantes Nieto
+#                                   <(°) 
+#                                     ( >)"
+#                                      /|
+####################################
+
+
+library(magrittr)
+c("dplyr","tidyr","lubridate","tseries", "astsa","forecast","ggplot2", 
+  "lattice", "dygraphs","XLConnect", "palettetown", "xts", "Rfacebook",
+  "twitteR") %>%  
+  sapply(require, character.only=T)
+
+
+setwd("~/local/TimeSeries18/")
+
+
+# Facebook ----------------------------------------------------------------
+
+
+datos1 <- read.csv("datos/facebook/2016.csv", header = T)
+
+datos1 <- datos1 %>% 
+  select(Date, Lifetime.Total.Likes, Daily.Total.Reach, Daily.Organic.Reach) %>% 
+  data.table %>% 
+  .[, Date := as.Date(Date)]
+
+
+
+archivos <- list.files("datos/facebook/", pattern = "*.xlsx", full.names = T)
+
+datos2 <- lapply(archivos, function(hoja){
+  Y <- read_excel(hoja, sheet = 1 ) %>% 
+    data.frame
+  names(Y)<- gsub(".Total", "", names(Y))
+  names(Y)<- gsub("Alcance.total.por.día", "Alcance.total.Diario", names(Y))
+  names(Y)<- gsub("Alcance.orgánico.por.día", "Alcance.orgánico.Diario",
+                  names(Y))
+  names(Y) <- iconv(names(Y), to = "ASCII//TRANSLIT")
+  Y <- Y %>%
+    select(Fecha, Total.de.Me.gusta, Alcance.total.Diario, 
+           Alcance.organico.Diario) %>%
+    data.table %>%
+    .[, Fecha := as.Date(Fecha)]
+  return(Y)
+}) %>% do.call("rbind", .)
+
+names(datos1)<- names(datos2)
+
+datosHomologaFB <- rbind(datos1, datos2) %>% 
+  unique
+
+#### datos raros de FB
+maxAlcance <- datosHomologaFB %>% 
+  group_by(Fecha) %>% 
+  summarise(alcanceMax = max(Alcance.organico.Diario))
+
+
+datosHomologaFB <- datosHomologaFB %>% 
+  left_join(maxAlcance, by="Fecha") %>% 
+  filter( Alcance.organico.Diario == alcanceMax) %>% 
+  select(- alcanceMax)
+
+
+datosHomologaFB %>% 
+  write.csv("datos/facebook/datosHomologadosFB.csv",
+            row.names=F)
+
+
+
+# Twitter -----------------------------------------------------------------
+datos1 <- read_excel("datos/twitter/TW - Engagement - SEAT México - 2017-09-25.xlsx",
+                     sheet = 3, skip = 1) %>% 
+  data.frame()
+
+datos2 <- read_excel("datos/twitter/TW - Followers - SEAT México - 2017-09-25.xlsx",
+                     sheet = 3, skip = 1) %>% 
+  data.frame()
+
+datos1 <- datos1 %>% 
+  select(Date, Likes, Replies, Retweets, Total.Interactions)
+
+datos2 <- datos2 %>% 
+  select(Date, Growth.of.Total.Followers)
+
+datosHomologaTW <- datos2 %>% 
+  left_join(datos1, by="Date")
+
+names(datosHomologaTW)<- c("Fecha", "Seguidores", "MeGusta", "Respuestas",
+                           "Retweets", "Totales")
+
+datosHomologaTW %>% 
+  write.csv("datos/twitter/datosHomologadosTW.csv",
+            row.names=F)
+
+
+# Instagram ---------------------------------------------------------------
+datos1 <- read_excel("datos/instagram/IG - Engagement - SEAT México - 2017-09-25.xlsx",
+                     sheet = 3, skip = 1) %>% 
+  data.frame()
+
+datos2 <- read_excel("datos/instagram/IG - Followers - SEAT México - 2017-09-25.xlsx",
+                     sheet = 3, skip = 1) %>% 
+  data.frame()
+
+datos1 <- datos1 %>% 
+  select(Date, Likes, Comments,  Total.Interactions)
+
+datos2 <- datos2 %>% 
+  select(Date, Growth.of.Total.Followers)
+
+datosHomologaIN <- datos2 %>% 
+  left_join(datos1, by="Date")
+
+names(datosHomologaIN) <- c("Fecha", "Seguidores", "MeGusta", "Comentarios", 
+                            "Totales")
+
+datosHomologaIN %>% 
+  write.csv("datos/instagram/datosHomologadosIN.csv",
+            row.names=F)
+
+
+
+# YouTube -----------------------------------------------------------------
+datos1 <- read.csv("datos/youtube/insights/suscritos.csv",
+                   header = T)
+datos2 <- read.csv("datos/youtube/visualizaciones/vistas.csv",
+                   header = T)
+
+datos1 <- datos1 %>% 
+  select(date, subscribers)
+
+datos2 <- datos2 %>% 
+  select(date, views)
+
+datosHomologaYT <- datos1 %>%
+  left_join(datos2, by="date")
+
+names(datosHomologaYT) <- c("Fecha", "Subscriptores", "Vistas")
+
+datosHomologaYT %>% 
+  write.csv("datos/youtube/datosHomologadosYT.csv",
+            row.names=F)
+
+
+
